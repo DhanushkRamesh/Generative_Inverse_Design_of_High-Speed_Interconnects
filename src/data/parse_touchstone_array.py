b19b1c7facbbe4c7f6a18b5bfee3d2de96fa0af1
DhanushkRamesh <dhanushkramesh@gmail.com>
2026-02-26 20:38:33 +0000

    feat: implement extraction and verification of TUHH SI/PI database - small initial simulation of the first sample

---
+++ src/data/parse_touchstone_array.py
@@ -0,0 +1,87 @@
+import os
+import glob
+import numpy as np 
+import pandas as pd
+import torch
+import skrf as rf
+from tqdm import tqdm
+
+def parse_touchstone_array(base_dir, output_dir):
+    print("Parsing Touchstone files...")
+    # Create output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+    csv_path=os.path.join(base_dir, "parameter.csv")
+    variation_dir=os.path.join(base_dir, "variation")
+    #load the full dataset
+    df=pd.read_csv(csv_path)
+    print(f"Loaded dataset with {len(df)} samples.")
+
+    sim_ids = df['SIMULATION'].values
+    features = df.drop(columns=['SIM_ID', 'SIMULATION'])
+    feature_names = features.columns.tolist()
+    feature_values = features.values
+    #min-max scaling of the features
+    X_raw = feature_values
+    X_min = X_raw.min(axis=0)
+    X_max = X_raw.max(axis=0)
+    range_values = np.where((X_max - X_min) == 0, 1e-5,(X_max - X_min))
+    X_scaled = (X_raw - X_min) / range_values
+    X_normalized= 2.0*X_scaled - 1.0
+    X_tensor = torch.tensor(X_normalized, dtype=torch.float32)
+
+    #extracting core features 4 port s-parameters
+    Y_real_list = []
+    Y_imag_list = []
+    valid_indexes = []
+    print(f"extracting s-parameters for {len(sim_ids)} simulations...")
+
+    for idx, sim_id in tqdm(enumerate(sim_ids), total=len(sim_ids)):
+        sim_folder = os.path.join(variation_dir, sim_id)
+        search_pattern = os.path.join(sim_folder, "*.s*p")
+        files = glob.glob(search_pattern)
+
+        if not files:
+            print(f"Warning: No Touchstone file found for simulation ID {sim_id} in {sim_folder}")
+            continue
+        touchstone_file = files[0]
+
+        try:
+            #load the network from the touchstone file
+            network = rf.Network(touchstone_file)
+            #extract the s-parameters and reshape to (4,4,201)
+            core_s_matrix=network.s[:,0:4,0:4]
+            s_matrix_real= torch.tensor(np.real(core_s_matrix), dtype=torch.float32)
+            s_matrix_imag= torch.tensor(np.imag(core_s_matrix), dtype=torch.float32)
+            Y_real_list.append(s_matrix_real)
+            Y_imag_list.append(s_matrix_imag)
+            valid_indexes.append(idx)
+
+            #testing it for first 50 simulations
+            if len(valid_indexes) >= 50:
+                print("Extracted s-parameters for 50 simulations, stopping further extraction for testing.")
+                break
+        except Exception as e:
+            print(f"Error processing Touchstone file for simulation ID {sim_id}: {e}")
+            continue
+        #Finilize tensors
+        X_final = X_tensor[valid_indexes]
+        Y_real=torch.stack(Y_real_list)
+        Y_imag=torch.stack(Y_imag_list)
+        #save the tensors
+        os.makedirs(output_dir, exist_ok=True)
+        save_path = os.path.join(output_dir, "via_array_dataset.pt")
+
+        torch.save({
+            'X': X_final,
+            'Y_real': Y_real,
+            'Y_imag': Y_imag,
+            'feature_names': feature_names,
+            'frequencies':torch.tensor(network.f, dtype=torch.float32)
+        }, save_path)
+    print(f"Dataset saved to {save_path}")
+    print(f"Final dataset shapes - X: {X_final.shape}, Y_real: {Y_real.shape}, Y_imag: {Y_imag.shape} (frequency points, 4, 4)")
+
+if __name__ == "__main__":
+    base_dir = os.path.expanduser("~/mece_project_inverse_model/Generative_Inverse_Design_of_High-Speed_Interconnects/data/raw/Universal-Diff-SI-Array")
+    output_dir = os.path.expanduser("~/mece_project_inverse_model/Generative_Inverse_Design_of_High-Speed_Interconnects/data/processed/Universal-Diff-SI-Array")
+    parse_touchstone_array(base_dir, output_dir)
